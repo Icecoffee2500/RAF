126ddeea60d82434776eab526cbb0a4378c55eb0
qwest <icecoffee2500@gmail.com>
2025-07-09 14:46:16 +0900

    test shell

---
+++ test_seg_fl_512x512x256_etri.sh.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# CHECKPOINT="checkpoints/cl-segformer-pretrained/best.pth"
+# CHECKPOINT="checkpoints/fl-segformer-256_256_256-gpu1_federated/best.pth"
+CHECKPOINT="checkpoints/fl-segformer-512_512_256-gpu2_federated/best.pth"
+# CHECKPOINT="checkpoints/fl-segformer-512_512_512-gpu3_federated/best.pth"
+
+DATA_ROOT="/home/user_cau/taeheon_ws/RAF/data/cityscapes"
+
+resolutions=(
+    "128 256"
+    "256 512" 
+    "512 1024"
+    "768 1536"
+    "1024 2048"
+)
+
+for res in "${resolutions[@]}"; do
+    echo "Testing at resolution: $res"
+    python -m raf.segmentation.fl_experiments.test \
+           --checkpoint $CHECKPOINT \
+           --resolution $res \
+           --data_root $DATA_ROOT
+    echo "---"
+done
