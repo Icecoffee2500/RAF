5f87e7127bc3d7ca87c75a17887f24723df03821
qwest <icecoffee2500@gmail.com>
2025-11-25 15:52:14 +0900

    test_hpe.py modified

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 64 48
-python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_fl_test.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 128 96
-python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_fl_test.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 192 144
-python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_fl_test.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 256 192
-python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_fl_test.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 320 240
-python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_fl_test.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 384 288
-python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_fl_test.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 512 384
+# python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 64 48
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 128 96
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 192 144
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 256 192
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 320 240
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 384 288
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_base-fulldata-hml-only_mr_mr_aug.pth --wandb --gpu 0 --test_res 512 384
