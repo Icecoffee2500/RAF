d0825d08a3ac5e09d14b4dd37d321a2d687a2d47
qwest <icecoffee2500@gmail.com>
2025-12-30 12:22:33 +0900

    test

--- test_uv_2.sh
+++ test_uv_2.sh
@@ -14,3 +14,10 @@ python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-sma
 python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_fedavg-4000-hml-no_kd.pth --wandb --gpu 7 --test_res 128 96 --test_interpolate --interpolate_im_shape 320 240
 python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_fedavg-4000-hml-no_kd.pth --wandb --gpu 7 --test_res 128 96 --test_interpolate --interpolate_im_shape 384 288
 python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_fedavg-4000-hml-no_kd.pth --wandb --gpu 7 --test_res 128 96 --test_interpolate --interpolate_im_shape 512 384
+
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_fedavg-4000-hml-no_kd.pth --wandb --gpu 7 --test_res 128 96
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_fedavg-4000-hml-no_kd.pth --wandb --gpu 7 --test_res 192 144
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_fedavg-4000-hml-no_kd.pth --wandb --gpu 7 --test_res 256 192
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_fedavg-4000-hml-no_kd.pth --wandb --gpu 7 --test_res 320 240
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_fedavg-4000-hml-no_kd.pth --wandb --gpu 7 --test_res 384 288
+python3 raf/experiments/test_hpe.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-small_256x192_192x144_128x96_sfl.yaml --pretrained output/fl_fedavg-4000-hml-no_kd.pth --wandb --gpu 7 --test_res 512 384
