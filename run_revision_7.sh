c2d9963f4f334b00ceae80bb0ff2b3b22835c268
qwest <icecoffee2500@gmail.com>
2026-01-20 11:59:27 +0900

    dataset changed to coco

--- run_revision_7.sh
+++ run_revision_7.sh
@@ -24,4 +24,7 @@ uv run raf/experiments/train_hpe_cl.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-
 # CL - vit-h / 576x432 - no kd
 uv run raf/experiments/train_hpe_cl.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-huge.yaml --pretrained ../pretrained/mae_pretrain_vit_huge.pth --wandb --gpu 6 --samples_per_client 12000 --train_bs 8 --test_bs 16 --loss_scale 1 --client_res max_high
 # CL - vit-h / 576x432 - mrkd
-uv run raf/experiments/train_hpe_cl.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-huge.yaml --pretrained ../pretrained/mae_pretrain_vit_huge.pth --wandb --gpu 0 --samples_per_client 12000 --train_bs 4 --test_bs 4 --loss_scale 1 --client_res max_high --kd_use --kd_alpha 0.5
+uv run raf/experiments/train_hpe_cl.py --cfg raf/configs/hpe/mpii_mpii_mpii/vit-huge.yaml --pretrained ../pretrained/mae_pretrain_vit_huge.pth --wandb --gpu 7 --samples_per_client 12000 --train_bs 8 --test_bs 16 --loss_scale 1 --client_res max_high --kd_use
+
+# CL - vit-h / 576x432 - no kd
+uv run raf/experiments/train_hpe_cl.py --cfg raf/configs/hpe/coco/vit/vit_small_multi_res.yaml --pretrained ../pretrained/mae_pretrain_vit_small.pth --wandb --gpu 0 --samples_per_client 0 --train_bs 64 --test_bs 128 --client_res high
